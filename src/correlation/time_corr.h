aa7e0be793a5a93a0b2c39bc527838b74a571441
Ivan-PIA <wind.vanya@gmail.com>
2024-08-08 02:42:00 +0700

    add symbol_sync + norm_corr

--- src/correlation/time_corr.h
+++ src/correlation/time_corr.h
@@ -3,7 +3,11 @@
 #include <complex> 
 #include <iterator>
 #include <fstream>
+#include <algorithm>
+#include <numeric>
 #include <cmath>
+#include <span>
+#include <chrono>
 
 using namespace std;
 
@@ -55,3 +59,80 @@ vector<double> convolve(vector<complex<double>>x, vector<complex<double>>h) {
 }
 
 
+double norm_corr(vector<complex<double>> x, vector<complex<double>> h){
+    
+
+    double norm_x;
+    double norm_h;
+
+    for(int i = 0; i < x.size(); i ++){
+        norm_x += abs(x[i]) * abs(x[i]);
+    }
+    for(int i = 0; i < h.size(); i ++){
+        norm_h += abs(h[i]) * abs(h[i]);
+    }
+
+    double correlate;
+    complex<double> t;
+    
+    for (int i = 0 ; i < x.size(); i++){
+        t = x[i] * h[i];
+        correlate += abs(t)/ (sqrt(norm_h) * sqrt(norm_x));
+    }
+    return correlate;
+}
+
+
+
+vector<int> symbol_sync(const std::vector<complex<double>> rx, int cp, int fft_len) {
+    std::vector<double> corr; // Массив корреляции 
+    std::vector<complex<double>> rx_copy = rx; // Копируем входной массив
+
+    for (size_t i = 0; i < rx.size() - fft_len - cp; i++) {
+        //auto start = std::chrono::high_resolution_clock::now();
+        double o = norm_corr(std::vector<complex<double>>(rx_copy.begin()+ i , rx_copy.begin() + cp + i), 
+                             std::vector<complex<double>>(rx_copy.begin() + fft_len + i , rx_copy.begin() + fft_len + cp + i));
+    
+
+        corr.push_back(o);
+        
+        // auto end = std::chrono::high_resolution_clock::now();
+        // auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        // cout << duration.count() << endl;
+    }
+
+
+    // Нормирование
+    double max_corr = *std::max_element(corr.begin(), corr.end());
+    for (auto& value : corr) {
+        value /= max_corr;
+    }
+
+    for(double ind1 : corr ){
+		// cout << ind1 << endl;
+	}
+
+    
+    size_t max_len_cycle = corr.size();
+    
+    size_t ind = std::distance(corr.begin(), std::max_element(corr.begin(), corr.begin() + (fft_len + cp) / 2));
+    
+    
+
+    std::vector<int> arr_index; 
+    arr_index.push_back(ind);
+    
+    for (size_t i = (fft_len + cp)/2 ; i < max_len_cycle; i += (fft_len + cp)) {
+        double max = *std::max_element(corr.begin() + i, corr.begin() + i + (fft_len + cp));
+        //cout << max << endl;
+        if (max > 0.90) {
+            ind = std::distance(corr.begin(), std::max_element(corr.begin() + i, corr.begin() + i + (fft_len + cp)));
+            //cout << ind<< " " << std::distance(corr.begin(), std::max_element(corr.begin() + i, corr.begin() + i + (fft_len + cp))) << endl;
+            if (ind < corr.size()) {
+                arr_index.push_back(ind);
+            }
+        }
+    }
+
+    return arr_index;
+}
